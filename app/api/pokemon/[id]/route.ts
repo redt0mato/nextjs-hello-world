d1a4bf4a39bdee8185d67652c433e3a3e6a512b8
redt0mato <3738vvawpoj3@gmail.com>
2023-07-22 16:13:37 -0700

    Added pokemon details page

--- app/api/pokemon/[id]/route.ts
+++ app/api/pokemon/[id]/route.ts
@@ -1,9 +1,7 @@
 import { NextResponse } from "next/server"
 
 export async function GET(request: Request) {
-  const resp = await fetch(
-    "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"
-  )
+  const resp = await fetch("https://pokeapi.co/api/v2/pokemon/1")
   const json = await resp.json()
 
   return NextResponse.json(json, { status: 200 })
--- app/pokemon/[id]/page.tsx
+++ app/pokemon/[id]/page.tsx
@@ -1,15 +1,47 @@
+"use client"
+
+import { useEffect, useState } from "react"
 import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 
-export default function IndexPage() {
+export default function IndexPage({ params }) {
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const resp = await fetch(`/api/pokemon/${params.id}`)
+      const json = await resp.json()
+      console.log(json)
+      setPokemon(json)
+    }
+    getData()
+  }, [])
+
+  debugger
+  if (!pokemon) return <div>Loading...</div>
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Pokemon
-        </h1>
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl"></h1>
+      </div>
+      <div className="flex flex-col">
+        <img
+          className="w-20 h-20"
+          src={pokemon.sprites && pokemon.sprites.front_default}
+        />
+        <div>
+          Name: <span>{pokemon.name}</span>
+        </div>
+        <div>
+          Abilities
+          <ul>
+            {pokemon.abilities.map((ability) => (
+              <li>{ability.ability.name}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
   )
