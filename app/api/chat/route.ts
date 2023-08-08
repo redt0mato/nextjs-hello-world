65e0eb423d497f5d741f5dbe530680dbe946f36d
redt0mato <3738vvawpoj3@gmail.com>
2023-08-07 22:58:16 -0700

    Remove debugger statements

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -7,15 +7,12 @@ export async function GET(request: Request) {
   const writer = responseStream.writable.getWriter()
   const encoder = new TextEncoder()
 
-  debugger
   writer.write(encoder.encode("Vercel is a platform for...."))
-  debugger
 
   const res = await fetch("http://127.0.0.1:5000/events")
 
   let bytes = 0
   for await (const chunk of res.body) {
-    debugger
     bytes += chunk.length
     console.log(
       `Chunk: ${new TextDecoder().decode(chunk)}. Read ${bytes} characters.`
